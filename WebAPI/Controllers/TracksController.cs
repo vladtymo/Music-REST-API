32bd7c1a8cb6689cb82fcf4485faf1e8036a3290
Vlad Tymoshchuk <Vlad@MAC-WINDOWS>
2022-08-31 16:33:57 +0300

    Add project files.

---
+++ WebAPI/Controllers/TracksController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TracksController : ControllerBase
+    {
+        public TracksController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("all")]           // localhost:port/api/tracks/all
+        //[Route("/get-tracks")] // localhost:port/get-tracks
+        public IActionResult GetAll()
+        {
+            return Ok(new
+            {
+                Id = 1,
+                Name = "Blue Sky",
+                Rating = 8.5,
+                Duration = "2:35"
+            });
+        }
+
+        // Get data from query parameters
+        [HttpGet]
+        public IActionResult GetWithQueryParam([FromQuery] int id) // query parameters
+        {
+            return Ok(new
+            {
+                Id = id,
+                Name = "Blue Sky",
+                Rating = 8.5,
+                Duration = "2:35"
+            });
+        }
+
+        // Get data from route parameters
+        //[HttpGet]
+        //[Route("{id:int}")] // route parameters
+        //public IActionResult GetWithRouteParam([FromRoute]int id)
+        //{
+        //    return Ok(new
+        //    {
+        //        Id = id,
+        //        Name = "Blue SKy",
+        //        Rating = 9.1,
+        //        Duration = "2:35"
+        //    });
+        //}
+
+        // Get data from body
+        [HttpGet("get-next")]
+        public IActionResult GetNextTrack([FromBody]Track track)
+        {
+            track.Id++;
+
+            return Ok(track);
+        }
+    }
+}
---
+++ WebAPI/Models/Track.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Models
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Rating { get; set; }
+        public string Duration { get; set; }
+    }
+}
